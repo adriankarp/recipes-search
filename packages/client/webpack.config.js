67e4529e82385feeaafe8716704806f74e8073c1
Adrian Karp <Adrian.Karp@ibm.com>
2023-02-06 11:01:16 +0200

    solved webpack error

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: './src/Index.tsx',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, './build'),
+    path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js'
+  },
+   optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCssAssetsPlugin({})],
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, './public')
+      directory: path.resolve(__dirname, './')
     }
   },
   module: {
@@ -22,10 +29,18 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.jsx', '.js']
   }
